65c26e81bc7c4a69d0e864731d7779c21d536dda
Akseli-Partanen <akseli.partanen@aalto.fi>
2016-06-13 10:57:09 +0300

    Added Test12.py

---
+++ Test12.py
@@ -0,0 +1,14 @@
+import sys
+
+def main():
+	sys.stdout.write('This program adds numbers.\n')
+	sum = 0
+	for arg in sys.argv:
+		try:
+			sum = sum + float(arg)
+		except:
+			continue
+	sys.stdout.write(str(sum))
+	
+
+main()
